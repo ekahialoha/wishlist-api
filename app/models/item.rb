a2730c8d9a4b59ecc37cd2e0f88ac7b54b23565e
Chris <chris@ekahi.co>
2019-06-07 15:27:07 -1000

    Added create model for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,21 @@ class Item
         DB = PG.connect(host: "localhost", port: 5432, dbname: 'wishlist-api_development')
     end
 
-    def self.create(input)
-        { create: true }
+    def self.create(opts)
+        results = DB.exec_params(<<-SQL, [opts['list_id'].to_i, opts['name'], opts['image'], opts['purchased'], opts['purchased_by']])
+            INSERT INTO items (list_id, name, image, purchased, purchased_by)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING id, list_id, name, image, purchased, purchased_by;
+        SQL
+        result = results.first
+        {
+            'id' => result['id'].to_i,
+            'list_id' => result['list_id'].to_i,
+            'name' => result['name'],
+            'image' => result['image'],
+            'purchased' => result == 't' ? true : false,
+            'purchased_by' => result['purchased_by']
+        }
     end
 
     def self.update(id, input)
