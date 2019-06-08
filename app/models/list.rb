78405f42dbe4ac9e797523898cbff54fb4014417
Molly Stone <molly.stone89@gmail.com>
2019-06-07 20:03:43 -0400

    Create list model and add index and show routes

--- app/models/list.rb
+++ app/models/list.rb
@@ -7,6 +7,55 @@ class List
     end
 
     def self.all
-        
+        results = DB.exec('SELECT w.* FROM wishlists w')
+
+        results.each do |result|
+            {
+                'id' => result['id'].to_i,
+                'name' => result['name'],
+                'description' => result['description'],
+                'image' => result['image']
+            }
+        end
+
+    end
+
+    def self.find id
+        results = DB.exec_params(<<-SQL, [id.to_i])
+            SELECT
+                w.*,
+                i.id AS items_id,
+                i.name,
+                i.image,
+                i.purchased,
+                i.purchased_by
+            FROM wishlists w
+            LEFT JOIN items i
+                ON w.id = i.list_id
+            WHERE w.id = $1
+        SQL
+
+        return {error: 'No results found'} if !results.first
+
+        result = results.first
+        items_list = []
+        results.each do |result|
+            if result['items_id']
+                items_list << {
+                    'id' => result['items_id'].to_i,
+                    'name' => result['name'],
+                    'image' => result['image'],
+                    'purchased' => result['purchased'] == 't' ? true : false,
+                    'purchased_by' => result['purchased_by']
+                }
+            end
+        end
+        {
+            'id' => result['id'].to_i,
+            'name' => result['name'],
+            'description' => result['description'],
+            'image' => result['image'],
+            'items' => items_list
+        }
     end
 end
